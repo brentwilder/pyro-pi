486df11156458b42b0573d2513957a32b1c9e00f
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-12-14 10:49:29 -0700

    Update example_processing.py

--- example_processing.py
+++ example_processing.py
@@ -1,9 +1,31 @@
 import pickle
+import glob
+import pandas as pd
+import numpy as np
 
-with open('./DATETIME_ht.pkl', 'rb') as f:
-    data = pickle.load(f)
-    time, rh, temp = zip(*data)
+# Saves the many, many pkl files created during Mobile-Pi collection and saves them to two clean csvs..
 
-with open('./DATETIME_pyr.pkl', 'rb') as f:
-    data = pickle.load(f)
-    print(data)
+data_list = []
+for filepath in glob.iglob('./MOBILE_PI/*_ht.pkl'):
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+        time, rh, temp = zip(*data)
+        time = time[0]
+        rh = np.mean(rh)
+        temp = np.mean(temp)
+        data_list.append([time,rh,temp])
+df = pd.DataFrame(data=data_list, columns=['Time', 'RH', 'Tair'])
+print(df)
+df.to_csv('./2022_1207_ht.csv')
+
+data_list = []
+for filepath in glob.iglob('./MOBILE_PI/*_pyr.pkl'):
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+        time = data[0][0]
+        p0_out = np.mean(data[4])
+        p1_out = np.mean(data[8])
+        data_list.append([time,p0_out,p1_out])
+df = pd.DataFrame(data=data_list, columns=['Time', 'P1', 'P2'])
+print(df)
+df.to_csv('./2022_1207_pyr.csv')
